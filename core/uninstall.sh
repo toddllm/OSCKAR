735243d74092305855ff47b7f8ecb8e6a2d348db
Patrick F. Wilbur <Patrick.Wilbur@gmail.com>
2009-10-07 18:14:25 +0000

    changed uninstall.sh so that it does not remove VM images during uninstall

--- core/uninstall.sh
+++ core/uninstall.sh
@@ -6,8 +6,12 @@ sudo rm -rf /usr/sbin/eventchat/ /usr/sbin/osckar /usr/sbin/osckar-core-debug /u
 
 sudo rm -rf /usr/share/osckar/
 
-sudo rm -rf /var/lib/osckar/ /var/lib/policymanager
+sudo rm -rf /var/lib/osckar/interfaces /var/lib/policymanager
 
 sudo rm -rf /var/log/eventchat /var/log/osckar /var/log/policymanager
 sudo rm -rf /var/run/eventchat /var/run/osckar /var/run/policymanager 
 
+echo 'OSCKAR uninstalled.'
+echo ''
+echo 'NOTE: Any images stored in /var/lib/osckar/images/ will not have been '
+echo '      deleted. Remove these files manually if you wish.'
