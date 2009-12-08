596e34fcb8643834648149d2614de704a175deaf
Chelsea Ross <rossca@clarkson.edu>
2009-12-08 21:26:03 +0000

    Added instructions at end of install.sh to remind user to restart Apache when user is able to in order to complete installation and enable web_kiosckar.

--- web_kiosckar/install.sh
+++ web_kiosckar/install.sh
@@ -6,8 +6,17 @@
 #install dependencies
 sudo apt-get install apache2 python
 
-
-
 #copy files
 sudo cp -r ./etc/* /etc/
 sudo cp -r ./var/* /var/
+
+#restart webserver (but don't ACTUALLY restart, just tell
+#  user to restart manually)
+echo 'web_kiosckar has been installed, BUT...'
+echo ' '
+echo 'You now need to restart Apache in order for new'
+echo 'settings to take effect.  Do this by running the'
+echo 'following command NOW (or when you are ready):'
+echo ' '
+echo '  sudo /etc/init.d/apache2 restart'
+echo ' '
