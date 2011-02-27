3d5f4a938aba40ab259b28f54e7426e048014d4b
Todd Deshane <deshantm@gmail.com>
2011-02-26 21:54:29 -0500

    fix install file to support other Ubuntu version add license blocks

--- core/install.sh
+++ core/install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# Copyright 2010 Todd Deshane <deshantm@gmail.com>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 cd `dirname $0`
 
 #detect supported platforms
 release=`grep DISTRIB_RELEASE /etc/lsb-release  | awk -F\= '{ print $2}'` 
-if [ X$release != X9.04 ] && [ X$release != X9.10 ]
+if [ X$release != X9.04 ] && [ X$release != X9.10 ] && [ X$release != X10.04 ] && [ X$release != X10.10 ]
 then
-    echo 'Ubuntu 9.04 and 9.10 are the only fully supported platform at this time'
+    echo 'Ubuntu is the only tested and supported platform for this release'
     exit 1
 fi
 
--- core/uninstall.sh
+++ core/uninstall.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2010 Todd Deshane <deshantm@gmail.com>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 sudo rm -rf /etc/init.d/osckar /etc/policymanager/
 
 sudo rm -rf /usr/sbin/eventchat/ /usr/sbin/osckar /usr/sbin/osckar-core-debug /usr/sbin/osckar-interface-builder /usr/sbin/osckar-interface-vmm /usr/sbin/policymanager
